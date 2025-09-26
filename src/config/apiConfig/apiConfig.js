dbae4777a539ff77318fd62ce6db01ec66b15615
Jay-1703 <pjay41907@gmail.com>
2025-09-26 12:21:17 +0530

    26-09-25

--- src/config/apiConfig/apiConfig.js
+++ src/config/apiConfig/apiConfig.js
@@ -1,17 +1,13 @@
 export const baseURL = process.env.REACT_APP_MAIN_BASE_URL
 
-export const timeZoneBaseURL = "https://timeapi.io/api"
-
-export const availableTimeZonesURL = `${timeZoneBaseURL}/timezone/availabletimezones`;
-export const currentTimeZoneURL = `${timeZoneBaseURL}/Time/current/zone?timeZone=`;
-
 export const faceRecognitionAPIBaseURL = process.env.REACT_APP_FACE_RECOGNITION_API_BASE_URL
 export const faceRecognitionModelURL = process.env.REACT_APP_FACE_RECOGNITION_MODEL_URL
 
-export const radarAPIURL = "https://api.radar.io/v1"
-export const radarSKAPIKey = "prj_live_sk_6d98dc8a1d5176a349d7226de83908980e1d9a12"
-export const radarPKAPIKey = "prj_live_pk_52c928de8697ee80316163c20c3708b53c3b3410"
-export const googleMapAPIKey = "AIzaSyBcUSPQk75SXsr0aoSXt-cJNt06bqhNMEo"
+export const radarAPIURL = process.env.REACT_APP_RADAR_API_BASE_URL
+export const radarSKAPIKey = process.env.REACT_APP_RADAR_SK_API_KEY
+export const radarPKAPIKey = process.env.REACT_APP_RADAR_PK_API_KEY
+
+export const googleMapAPIKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY
 
 export const timeZoneURL = baseURL + '/getTimezones'
 export const userURL = baseURL + '/user'
